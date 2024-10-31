27a4cdb0ed611587ec437385feb2be464b02aebc
Gerrishonngugi <gerrishonngugi@gmail.com>
2024-10-31 10:28:34 +0300

    task_3(pending corrections)

---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    header: './modules/header/header.js',
+    body: './modules/body/body.js',
+    footer: './modules/footer/footer.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 8564,
+    open: true,
+    static: {
+      directory: path.join(__dirname, 'public'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './modules/index.html'
+    })
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
+};
