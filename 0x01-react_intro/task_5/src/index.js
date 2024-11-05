bfdf1bc1707388b484caef78bca49572c4753a15
Gerrishonngugi <gerrishonngugi@gmail.com>
2024-11-05 10:21:46 +0300

    task_5

--- 0x01-react_intro/task_5/src/index.js
+++ 0x01-react_intro/task_5/src/index.js
@@ -5,10 +5,12 @@ import Notifications from "./Notifications/Notifications";
 
 ReactDOM.render(
   <React.StrictMode>
-    <div id="root-notifications">
-      <Notifications />
-    </div>
-    <App />
+    <>
+      <div id="root-notifications">
+        <Notifications />
+      </div>
+      <App />
+    </>
   </React.StrictMode>,
   document.getElementById("root")
 );
